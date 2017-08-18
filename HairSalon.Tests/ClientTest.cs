7f58db7f101b3cf035dd28e0a62c7f93cddd13e3
Kaili Nishihira <kailinishihira@gmail.com>
2017-08-18 10:07:04 -1000

    add get all, equals, saves methods to client.cs

--- HairSalon.Tests/ClientTest.cs
+++ HairSalon.Tests/ClientTest.cs
@@ -6,11 +6,79 @@ using System;
 namespace HairSalon.Tests
 {
   [TestClass]
-  public class ClientTest
+  public class ClientTest : IDisposable
   {
     public ClientTest()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=3306;database=hair_salon_test;";
     }
+
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+      Client.DeleteAll();
+    }
+
+    [TestMethod]
+    public void GetAll_ClientsEmptyAtFirst_0()
+    {
+      int result = Client.GetAll().Count;
+      Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void Equals_ClientsAreEqual_true()
+    {
+      Client newClient = new Client("Abby", "Kline", "303-555-1234", "abby.kline@gmail.com", 1);
+      Client newClient2 = new Client("Abby", "Kline", "303-555-1234", "abby.kline@gmail.com", 1);
+      Assert.AreEqual(newClient, newClient2);
+    }
+
+    [TestMethod]
+    public void Save_AssignsIdToObject_Id()
+    {
+      Client testClient = new Client("Abby", "Kline", "303-555-1234", "abby.kline@gmail.com", 1);
+      testClient.Save();
+      Client savedClient = Client.GetAll()[0];
+      int result = savedClient.GetId();
+      int testId = testClient.GetId();
+      Assert.AreEqual(testId, result);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_ClientList()
+    {
+      Client newClient = new Client("Abby", "Kline", "303-555-1234", "abby.kline@gmail.com", 1);
+      newClient.Save();
+      List<Client> result = Client.GetAll();
+      List<Client> expected = new List<Client>{newClient};
+      Console.WriteLine(newClient.GetDetails());
+      CollectionAssert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void GetAll_Lists2Clients_Clients()
+    {
+      Client newClient1 = new Client("Abby", "Kline", "303-555-1234", "abby.kline@gmail.com", 1);
+      newClient1.Save();
+      Client newClient2 = new Client("Sam", "Ruth", "206-555-1234", "sam.ruth@gmail.com", 2);
+      newClient2.Save();
+      int result = Client.GetAll().Count;
+      Assert.AreEqual(2, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ListofClients_ClientList()
+    {
+      Client newClient1 = new Client("Abby", "Kline", "303-555-1234", "abby.kline@gmail.com", 1);
+      newClient1.Save();
+      Client newClient2 = new Client("Sam", "Ruth", "206-555-1234", "sam.ruth@gmail.com", 2);
+      newClient2.Save();
+      List<Client> allClients = Client.GetAll();
+      List<Client> expectedList = new List<Client>{newClient1, newClient2};
+      CollectionAssert.AreEqual(allClients, expectedList);
+    }
+
+
   }
 }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -23,6 +23,147 @@ namespace HairSalon.Models
       _stylistId = stylistId;
     }
 
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetFirstName()
+    {
+      return _firstName;
+    }
+
+    public string GetLastName()
+    {
+      return _lastName;
+    }
+
+    public string GetPhone()
+    {
+      return _phone;
+    }
+
+    public string GetEmail()
+    {
+      return _email;
+    }
+
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+
+    public string GetDetails()
+   {
+     return "ID: " +_id + ", First Name: " + _firstName + ", Last Name: " + _lastName + ", phone: " + _phone + ",  Email: " + _email + ", StylistID: " + _stylistId;
+   }
+
+   public override bool Equals(System.Object otherClient)
+  {
+    if(!(otherClient is Client))
+    {
+      return false;
+    }
+    else
+    {
+      Client newClient = (Client) otherClient;
+      bool idEquality = (this.GetId() == newClient.GetId());
+      bool firstNameEquality = (this.GetFirstName() == newClient.GetFirstName());
+      bool lastNameEquality = (this.GetLastName() == newClient.GetLastName());
+      bool phoneEquality = (this.GetPhone() == newClient.GetPhone());
+      bool emailEquality = (this.GetEmail() == newClient.GetEmail());
+      bool stylistIdEquality = (this.GetStylistId() == newClient.GetStylistId());
+      return (idEquality && firstNameEquality && lastNameEquality && phoneEquality && emailEquality && stylistIdEquality);
+    }
+  }
+
+  public override int GetHashCode()
+  {
+    return this.GetFirstName().GetHashCode();
+  }
+
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM clients;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client>();
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText= @"SELECT * FROM clients ORDER BY last_name ASC;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int ClientId = rdr.GetInt32(0);
+        string ClientFirstName = rdr.GetString(1);
+        string ClientLastName = rdr.GetString(2);
+        string ClientPhone = rdr.GetString(3);
+        string ClientEmail = rdr.GetString(4);
+        int StylistId = rdr.GetInt32(5);
+        Client newClient = new Client(ClientFirstName, ClientLastName, ClientPhone, ClientEmail, StylistId, ClientId);
+        allClients.Add(newClient);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allClients;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO clients(first_name, last_name, phone, email, stylist_id) VALUES(@firstName, @lastName, @phone, @email, @stylistId);";
+
+      MySqlParameter firstName = new MySqlParameter();
+      firstName.ParameterName = "@firstName";
+      firstName.Value = this._firstName;
+      cmd.Parameters.Add(firstName);
+
+      MySqlParameter lastName = new MySqlParameter();
+      lastName.ParameterName = "@lastName";
+      lastName.Value = this._lastName;
+      cmd.Parameters.Add(lastName);
+
+      MySqlParameter phone = new MySqlParameter();
+      phone.ParameterName = "@phone";
+      phone.Value = this._phone;
+      cmd.Parameters.Add(phone);
+
+      MySqlParameter email = new MySqlParameter();
+      email.ParameterName = "@email";
+      email.Value = this._email;
+      cmd.Parameters.Add(email);
+
+      MySqlParameter stylistId = new MySqlParameter();
+      stylistId.ParameterName = "@stylistId";
+      stylistId.Value = this._stylistId;
+      cmd.Parameters.Add(stylistId);
+
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
 
   }
 }
